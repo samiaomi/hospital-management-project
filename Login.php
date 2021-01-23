08f54f28f1aaaf570d39cf59593fe12334a5bf3c
samiaomi <68068820+samiaomi@users.noreply.github.com>
2021-01-23 11:24:04 +0600

    Add files via upload

---
+++ Login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+  <html lang="en" dir="ltr">
+      <head>
+        <meta charset="utf-8">
+        <title>Home</title>
+        <link rel="stylesheet" href="home.css" type="text/css">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+      </head>
+      <body>
+        <div class="w3-bar w3-black" style="height:36px;">
+          <a href="index.php" class="w3-bar-item w3-button">Home</a>
+          <a href="about.php" class="w3-bar-item w3-button">About page</a>
+          <a href="Login.php" class="w3-bar-item w3-button">Login</a>
+        </div>
+        <h1>Welcome to care and cure hospital.</h1>
+        <h2>Login in order to continue.</h2>
+        <center><div class="button">
+          <a href="admin/Admin_login.php" class="login">Admin Login</a>
+
+          <a href="doctor/Doctor_login.php" class="login">Doctor Login</a>
+
+          <a href="patient/Patient_login.php" class="login">Patient Login</a>
+
+          <a href="employee/Employee_login.php" class="login">Employee Login</a>
+        </div>
+      </center>
+      </body>
+  </html>
---
+++ index.php
@@ -0,0 +1,36 @@
+<html>
+    <head>
+        <title>Care and Cure Hospital</title>
+        <link rel = "stylesheet" type = "text/css" href = "WelcomePage.css">
+        <link rel = "stylesheet" type = "text/css" href = "all.css">
+    </head>
+
+    <body style="background-image: url('images/bg.jpg');">
+
+
+        <div class="background">
+          <div style="background-color: #BCC6CC;">
+            <center><b><h1 style="color: darkslateblue; font-family: fantasy; font-size: 500%; font-style: inherit; ">Welcome to Care and Cure Hospital</h1></b><br></center>
+          </div>
+            <div class="nav">
+                <a href="about.php">About Hospital</a><br>
+                <a href="searchdoctor.php">About Doctors</a><br>
+                <a href="Login.php">Login</a><br>
+                <a href="Register.php">Register</a><br>
+
+                <button onclick="document.getElementById('demo').innerHTML=Date()">The time is?</button>
+                <p id="demo"></p>
+              </div>
+              <div class="contact">
+              		<p><h4>Contact:</h4><br><a href="#"><i class="fas fa-phone-alt"></i></a>Phone Number:01816210605,01949328244
+              		<br><a href="#"><i class="fab fa-facebook-f"></i></a>https://www.facebook.com/care_and_cure/
+              		<br><a href="#"><i class="fas fa-envelope"></i></a>info@care_and_cure.Org
+              		<br><a href="#"><i class="fas fa-map-marker-alt"></i></a>Mirpur Cantonment, Road No 2 Section# 12,1216
+              		</p>
+              		<p>© All rights reserved Care_and_Cure.org</p>
+	            </div>
+
+        </div>
+
+    </body>
+</html>
